e549e597bb80e8c2cc5fad6465d9508961ac785c
Paul Hamshere <p.w.hamshere@gmail.com>
2022-02-17 16:56:24 +0000

    Create Spark.py

---
+++ src/Spark.py
@@ -0,0 +1,106 @@
+from __future__ import print_function
+import cb
+import sound
+import time
+import struct
+
+import socket
+
+class SparkManager (object):
+    def __init__(self):
+        self.peripheral = None
+        self.midi=None
+        self.send_char = None
+        self.recv_char = None
+
+    def did_discover_peripheral(self, p):
+        if p.name and 'Spark 40 BLE' in p.name and not self.peripheral:
+            self.peripheral = p
+            print('Connecting Spark')
+            cb.connect_peripheral(p)
+
+        """        
+        if p.name and 'Akai LPD8 Wireless' in p.name and not self.midi:
+            self.midi=p
+            print('Connecting Akai')
+            cb.connect_peripheral(p)
+        """  	
+
+    def did_connect_peripheral(self, p):
+        print('Connected:', p.name)
+        print('Discovering services...')
+        p.discover_services()
+
+    def did_fail_to_connect_peripheral(self, p, error):
+        print('Failed to connect: %s' % (error,))
+
+    def did_disconnect_peripheral(self, p, error):
+        print('Disconnected, error: %s' % (error,))
+        self.peripheral = None
+
+    def did_discover_services(self, p, error):
+        for s in p.services:
+            print(s.uuid)
+            if s.uuid == 'FFC0':
+                print('Discovered FFC0')
+                p.discover_characteristics(s)
+
+    def did_discover_characteristics(self, s, error):
+        print('Did discover characteristics...')
+        for c in s.characteristics:
+            if c.uuid == 'FFC2' and self.recv_char == None:
+                self.recv_char = c
+                print(c.uuid)
+                self.peripheral.set_notify_value(c, True)
+            if c.uuid == 'FFC1' and self.send_char == None:
+                self.send_char = c
+                print(c.uuid)
+
+    def did_update_value(self, c, error):
+        #for x in c.value:
+            #print(' %x' % x, end='')
+        #print('\n')
+        a=1
+
+    def send_bytes(self, data):
+        self.peripheral.write_characteristic_value(self.send_char, data, False)
+        #print("Sent ", data)
+
+mngr = SparkManager()
+cb.set_central_delegate(mngr)
+print('Scanning for peripherals...')
+cb.scan_for_peripherals()
+
+import socket
+
+s = socket.socket(
+    socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(("0.0.0.0", 5555))
+
+
+tone1="01fe000053fe1a000000000000000000f00124000138000000f7"
+tone2="01fe000053fe1a000000000000000000f00123010138000001f7"
+tone3="01fe000053fe1a000000000000000000f00125020138000002f7"
+tone4="01fe000053fe1a000000000000000000f00120030138000003f7"
+
+tone1b=bytes.fromhex(tone1)
+tone2b=bytes.fromhex(tone2)
+tone3b=bytes.fromhex(tone3)
+tone4b=bytes.fromhex(tone4)
+
+while True:
+    d=s.recv(20)
+    if d[0]==0x90:
+        if d[1]==0x18:
+           mngr.send_bytes(tone1b)
+           print("Preset 1")
+        elif d[1]==0x1a:
+           mngr.send_bytes(tone2b)
+           print("Preset 2")
+        elif d[1]==0x1c:
+           mngr.send_bytes(tone3b)
+           print("Preset 3")
+        elif d[1]==0x1d:
+           mngr.send_bytes(tone4b)
+           print("Preset 4")
+
